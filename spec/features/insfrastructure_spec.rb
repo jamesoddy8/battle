44f4f93f048718ba334e2adca03f899acf03c74c
James Oddy <jamesoddy8@gmail.com>
2020-03-04 18:05:38 +0000

    completed 'skinny controllers'

--- spec/features/insfrastructure_spec.rb
+++
@@ -1,8 +0,0 @@
-
-
-# feature 'homepage' do
-#   scenario 'expects homepage to display: Testing infrastructure working!' do
-#     visit('/')
-#     expect(page).to have_content "Testing infrastructure working!"
-#   end
-# end
